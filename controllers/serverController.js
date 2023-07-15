ed31015ba7d85372608a241792e5c37b0400f090
Nabil Ahmed <nabilahmed.great@gmail.com>
2023-07-15 09:46:30 +0600

    Pushing the changes

--- controllers/serverController.js
+++ controllers/serverController.js
@@ -1,28 +1,29 @@
 const session = require("express-session");
 const redisClient = require("../redis");
 const RedisStore = require("@goberman/connect-redis")(session);
-require("dotenv").config()
+require("dotenv").config();
 
 const sessionMiddleWare = session({
-    secret:process.env.COOKIE_SECRET,
-    credentials:true,
-    name:"$id",
-    store:new RedisStore({client:redisClient}),
-    resave:false,
-    saveUninitialized:false,
-    cookie: {
-        secure:process.env.NODE_ENV === "production" ? "true" : "auto",
-        httpOnly:true,
-        expires:1000 * 60 * 60 * 24 * 7,
-        sameSite:process.env.NODE_ENV === "production" ? "none" : "lax",
-    }
-})
+  secret: process.env.COOKIE_SECRET,
+  credentials: true,
+  name: "$id",
+  store: new RedisStore({ client: redisClient }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === "production" ? "true" : "auto",
+    httpOnly: true,
+    expires: 1000 * 60 * 60 * 24 * 7,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  },
+});
 
-const wrap = (expressMiddleWare) => (socket,next) => expressMiddleWare(socket.request,{},next)
+const wrap = (expressMiddleWare) => (socket, next) =>
+  expressMiddleWare(socket.request, {}, next);
 
 const corsConfig = {
-    origin:`https://chat-pheonix.netlify.app`,
-    credentials:true,
-}
+  origin:"https://chat-pheonix.netlify.app",
+  credentials: true,
+};
 
-module.exports = {sessionMiddleWare,wrap,corsConfig}
+module.exports = { sessionMiddleWare, wrap, corsConfig };
