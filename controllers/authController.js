882e3516636ddc3432d9f5c51838be305ce79c0a
Nabil Ahmed <nabilahmed.great@gmail.com>
2023-07-21 22:36:10 +0600

    Arranging the whole code according to best practices

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,80 +1,9 @@
-const pool = require("../db");
-const bcrypt = require("bcrypt");
-const {v4 : uuid4} = require("uuid");
-const { jwtSign, jwtVerify, getJwt } = require("./jwt/jwtAuth");
-require("dotenv").config()
+const attemptLogin = require("./express/attemptLogin");
+const attemptRegister = require("./express/attemptRegister");
+const handleLogin = require("./express/handleLogin");
 
-module.exports.handleLogin = (req,res) => {
-    const token = getJwt(req);
-
-    if(!token) {
-        res.json({loggedIn:false});
-        return;
-    }
-
-    jwtVerify(token, process.env.JWT_SECRET)
-    .then(() => {
-        res.json({loggedIn:true, token})
-    }).catch(err => {
-        console.log(err)
-        res.json({loggedIn:false})
-    })
-}
-
-module.exports.attemptLogin = async (req,res) => {
-    const potentialLogin = await pool.query(
-        "SELECT id,username,passhash,userid FROM users u WHERE u.username=$1", [req.body.username])
-
-    if(potentialLogin.rowCount > 0) {
-        const isSamePass = await bcrypt.compare(req.body.password, potentialLogin.rows[0].passhash);
-    
-        if(isSamePass) {
-            jwtSign({
-                username:req.body.username,
-                id:potentialLogin.rows[0].id,
-                userid:potentialLogin.rows[0].userid
-            }, process.env.JWT_SECRET, 
-            {
-                expiresIn:"7d"
-            }).then(token => {
-                res.json({loggedIn:true, token})
-            }).catch(err => {
-                console.log(err);
-                res.json({loggedIn:false, status: "Something went wrong, try again later"})
-            }) 
-        }else {
-        console.log("Not good")
-        res.json({loggedIn:false,status:"Wrong username or password"})
-        } }else {
-        console.log("Not good")
-        res.json({loggedIn:false,status:"Wrong username or password"})
-    }
-}
-
-module.exports.attemptRegister = async (req,res) => {
-    const existingUser = await pool.query("SELECT username FROM users WHERE username = $1", 
-    [req.body.username]);
-
-    if(existingUser.rowCount === 0) {
-        const hashedPass = await bcrypt.hash(req.body.password, 10);
-        const newUserQuery = await pool.query(
-            "INSERT INTO users(username,passhash,userid) VALUES($1,$2,$3) RETURNING id,username,userid",
-            [req.body.username, hashedPass,uuid4()]
-            );
-             jwtSign({
-                username:req.body.username,
-                id:newUserQuery.rows[0].id,
-                userid:newUserQuery.rows[0].userid
-            }, process.env.JWT_SECRET, 
-            {
-                expiresIn:"7d"
-            }).then(token => {
-                res.json({loggedIn:true, token})
-            }).catch(err => {
-                console.log(err);
-                res.json({loggedIn:false, status: "Something went wrong, try again later"})
-            }) 
-    }else {
-        res.json({loggedIn:false, status:"Username already taken"})
-    }
-}
+module.exports = {
+  attemptLogin,
+  attemptRegister,
+  handleLogin,
+};
--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,98 +1,13 @@
-const redisClient = require("../redis");
-const { jwtVerify } = require("./jwt/jwtAuth");
-require("dotenv").config();
+const authorizeUser = require("./socketio/authorizeUser");
+const initializeUser = require("./socketio/initializeUser");
+const addFriend = require("./socketio/addFriend");
+const onDisconnect = require("./socketio/onDisconnect");
+const dm = require("./socketio/dm");
 
-module.exports.authorizeUser = (socket,next) => {
-    const token = socket.handshake.auth.token;
-
-    jwtVerify(token, process.env.JWT_SECRET)
-    .then(decoded => {
-        socket.user = {...decoded};
-        next()
-    }).catch(err => {
-        console.log("Bad request",  err)
-        next(new Error("Not authorized"))
-    })
-}
-
-module.exports.initializeUser = async socket => {
-    socket.join(socket.user.userid)
-    await redisClient.hset(`userid:${socket.user.username}`, "connected", true);
-    await redisClient.hset(`userid:${socket.user.username}`, "userid", socket.user.userid);
-    const friendList = await redisClient.lrange(`friends:${socket.user.username}`, 0, -1)
-    const parsedFriendList = await parseFriendList(friendList)
-    const friendRooms = parsedFriendList.map(friend => friend.userid);
-    socket.to(friendRooms).emit("connected",true, socket.user.username)
-    socket.emit("friends", parsedFriendList)
-    const messageQuery = await redisClient.lrange(`chat:${socket.user.userid}`, 0, -1);
-    const messagesAndDate = messageQuery.map(msgStr => {
-        const parsedStr = msgStr.split(".");
-        return {to:parsedStr[0],from:parsedStr[1],content:parsedStr[2],time:parsedStr[3]};
-    });
-    if(messagesAndDate && messagesAndDate.length > 0) {
-        socket.emit("messages", messagesAndDate)
-    }
-}
-
-module.exports.addFriend = async (socket,friendname,cb) => {
-    if(friendname === socket.user.username) {
-        cb({done:false,errorMsg:"Cannot add youself as a friend"})
-        return;
-    }
-    const friend = await redisClient.hgetall(`userid:${friendname}`)
-
-    const currentFriendList = await redisClient.lrange(`friends:${socket.user.username}`, 0, -1)
-
-    if(!friend.userid) {
-        cb({done:false,errorMsg:"User doesn't exist"})
-        return;
-    }
-    if (currentFriendList && currentFriendList.some(friend => friend.startsWith(`${friendname}.`))) {
-        cb({done:false,errorMsg:"Friend already added"})
-        return;
-    }
-    await redisClient.lpush(`friends:${socket.user.username}`, [friendname,friend.userid].join("."))
-    const newFriend = {
-        username:friendname,
-        userid:friend.userid,
-        connected:friend.connected,
-    }
-    cb({done:true,newFriend})
-}
-
-
-module.exports.onDisconnect = async (socket) => {
-    await redisClient.hset(`userid:${socket.user.username}`, "connected", false);
-
-    const friendList = await redisClient.lrange(`friends:${socket.user.username}`, 0, -1)
-    const friendRooms = await parseFriendList(friendList)
-    const socketIDs = friendRooms.map(friend => friend.userid);
-    socket.to(socketIDs).emit("connected",false, socket.user.username)
-
-}
-
-module.exports.dm = async (socket,message) => {
-    message.from = socket.user.userid;
-
-    const messageString = [message.to, message.from,message.content,message.time].join(".");
-
-    await redisClient.lpush(`chat:${message.to}`, messageString)
-    await redisClient.lpush(`chat:${message.from}`, messageString)
-
-    
-    socket.to(message.to).emit("dm", message);
-}
-
-const parseFriendList = async (friendList) => {
-    const newFriendList = [];
-    for(let friend of friendList) {
-        const parsedFriend = friend.split(".");
-        const friendConnected = await redisClient.hget(`userid:${parsedFriend[0]}`, "connected")
-        newFriendList.push({
-            username:parsedFriend[0],
-            userid:parsedFriend[1],
-            connected:friendConnected,
-        })
-    }
-    return newFriendList;
-}
+module.exports = {
+  addFriend,
+  authorizeUser,
+  initializeUser,
+  onDisconnect,
+  dm,
+};
