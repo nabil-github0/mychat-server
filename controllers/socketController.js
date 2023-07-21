714eae3865b0a91dd3cbf8f7c245a67761660954
Nabil Ahmed <nabilahmed.great@gmail.com>
2023-07-21 18:26:35 +0600

    Switching session to jwt

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -5,8 +5,6 @@ require("dotenv").config();
 module.exports.authorizeUser = (socket,next) => {
     const token = socket.handshake.auth.token;
 
-    console.log(token)
-
     jwtVerify(token, process.env.JWT_SECRET)
     .then(decoded => {
         socket.user = {...decoded};
